2215ba7367216ac23cf912cfb2afe0241271b55e
bdebon <benjamin.debon@gmail.com>
2023-06-02 15:56:45 +0200

    fix am pm

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import './style.css'
 
-const percent = 1
-
 // Paris
 const latitude = 48.866667
 const longitude = 2.333333
 
-const url = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}`
+const url = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&formatted=0`
 
 
 // create a datetime half an hour after the current date
@@ -21,37 +19,40 @@ fetch(url).then(res => res.json()).then((data: {
   "status": string
 }) => {
   if (data.results?.sunset) {
-    sunset = new Date()
-    const sunsetTime = data.results.sunset.split(':')
-    // create a date from this time "7:45:11 PM"
-    sunset.setHours(parseInt(sunsetTime[0]) + 1)
-    sunset.setMinutes(parseInt(sunsetTime[1]))
-    sunset.setSeconds(parseInt(sunsetTime[2].split(' ')[0]))
+    sunset = new Date(data.results.sunset)
 
+    sunset = new Date()
+    // add 30 minutes to the sunset
+    sunset.setMinutes(sunset.getMinutes() + 70)
     updateOpacity()
   }
 })
 
+
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <div class="bg bg--night" style="background-image: url('5.jpg')"></div>
-    <div class="bg bg--light" style="background-image: url('1.jpg'); opacity: ${percent}"></div>
+    <div class="bg bg--night" style="background-image: url('/5.jpg')"></div>
+    <div class="bg bg--light" style="background-image: url('/1.jpg');"></div>
   </div>
 `
 
 function computeOpacity() {
   const now = new Date()
 
+  let sunsetHours = sunset.getHours();
+
+
   // if now is one hour before sunset, opacity is 1
-  if (now.getHours() === sunset.getHours() - 1) {
+  if (now.getHours() <= sunsetHours - 1) {
     return 1
-  } else if (now.getHours() >= sunset.getHours() && now.getMinutes() >= sunset.getMinutes()) {
+  } else if (now.getHours() >= sunsetHours && now.getMinutes() >= sunset.getMinutes()) {
     // if now is sunset, opacity is 0
     return 0
   }
 
   // otherwise, compute opacity
-  const totalMinutes = (sunset.getHours() - now.getHours()) * 60 + (sunset.getMinutes() - now.getMinutes())
+  const totalMinutes = (sunsetHours - now.getHours()) * 60 + (sunset.getMinutes() - now.getMinutes())
   const minutesPassed = 60 - totalMinutes
   return minutesPassed / 60
 }
@@ -62,4 +63,4 @@ const updateOpacity = () => {
   document.querySelector<HTMLDivElement>('.bg--light')!.style.opacity = opacity.toString()
 }
 
-setInterval(updateOpacity, 10000)
+//setInterval(updateOpacity, 10000)
